18ed2cab5e49de735000de21de89f7cef7b6e4c7
Matthew Smart <matthewsmart13@gmail.com>
2023-03-24 16:31:24 -0500

    Updated import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Education from "./components/Education";
 import Employment from "./components/Employment";
-import Form from "./components/Form";
+import Form from "./components/GeneralForm";
 
 class App extends Component {
 	constructor() {
