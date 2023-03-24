9add2654cf0f827c3c517ba53c4b16ec8beffa08
Matthew Smart <matthewsmart13@gmail.com>
2023-03-24 16:07:28 -0500

    Removed state and changed to use props from form

--- src/components/General.js
+++ src/components/General.js
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
 
 class General extends Component {
-	constructor() {
-		super();
-		this.state = {
-			name: "Jone Doe",
-			phone: "(123) 123-1234",
-			email: "jonedoe123@gmail.com",
-		};
-	}
-
 	render() {
 		return (
 			<div>
 				<div>
-					<p>{this.state.name}</p>
-					<p>Phone: {this.state.phone}</p>
-					<p>Email: {this.state.email}</p>
+					<p>{this.props.info.name}</p>
+					<p>Phone: {this.props.info.phone}</p>
+					<p>Email: {this.props.info.email}</p>
+					<button onClick={this.props.func}>Edit</button>
 				</div>
 			</div>
 		);
