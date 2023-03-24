703a3868ecbab9e35d7021c60bed10902d773e1f
Matthew Smart <matthewsmart13@gmail.com>
2023-03-24 13:29:15 -0500

    Made a file for Education

---
+++ src/components/Education.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Education extends Component {
+	constructor() {
+		super();
+		this.state = {
+			school: "Test School",
+			study: "Computer Information Science",
+			startDate: "2013",
+			endDate: "2015",
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<div>
+					<p>{this.state.school}</p>
+					<p>Degree: {this.state.study}</p>
+					<p>Start Date: {this.state.startDate}</p>
+					<p>End Date: {this.state.endDate}</p>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Education;
