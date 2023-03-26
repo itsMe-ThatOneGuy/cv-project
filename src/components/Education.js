f45a23c56ed93bd82255ee48772e501f284c91fc
Matthew Smart <matthewsmart13@gmail.com>
2023-03-25 19:27:56 -0500

    Updated render to use the updated state

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,13 +4,17 @@ class Education extends Component {
 	render() {
 		return (
 			<div>
-				<div>
-					<p>{this.props.info.school}</p>
-					<p>Degree: {this.props.info.degree}</p>
-					<p>Start Date: {this.props.info.startDate}</p>
-					<p>End Date: {this.props.info.endDate}</p>
-					<button onClick={this.props.func}>Edit</button>
-				</div>
+				{this.props.info.schools.map((school) => {
+					return (
+						<div key={school.id++}>
+							<p>{school.schoolName}</p>
+							<p>Degree: {school.degree}</p>
+							<p>Start Date: {school.startDate}</p>
+							<p>End Date: {school.endDate}</p>
+							<button>Edit</button>
+						</div>
+					);
+				})}
 			</div>
 		);
 	}
