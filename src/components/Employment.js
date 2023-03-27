a76c9b32cf2a7887bbb33ae47ce864bb76173779
Matthew Smart <matthewsmart13@gmail.com>
2023-03-27 12:46:03 -0500

    Updated Render function added function to delete

--- src/components/Employment.js
+++ src/components/Employment.js
@@ -4,14 +4,25 @@ class Employment extends Component {
 	render() {
 		return (
 			<div>
-				<div>
-					<p>{this.props.info.employer}</p>
-					<p>Title: {this.props.info.title}</p>
-					<p>Responsibilities: {this.props.info.responsibilities}</p>
-					<p>Start Date: {this.props.info.startDate}</p>
-					<p>End Date: {this.props.info.endDate}</p>
-					<button onClick={this.props.func}>Edit</button>
-				</div>
+				{this.props.info.jobs.map((job) => {
+					return (
+						<div key={job.id}>
+							<p>{this.props.info.employer}</p>
+							<p>Title: {this.props.info.title}</p>
+							<p>Responsibilities: {this.props.info.responsibilities}</p>
+							<p>Start Date: {this.props.info.startDate}</p>
+							<p>End Date: {this.props.info.endDate}</p>
+							<button onClick={this.props.editOnClick} id={job.id}>
+								Edit
+							</button>
+							<button
+								onClick={this.props.deleteOnClick}
+								id={`delete-${job.id}`}>
+								Delete
+							</button>
+						</div>
+					);
+				})}
 			</div>
 		);
 	}
