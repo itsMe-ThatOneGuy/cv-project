f3af7207a67335a08d930a77ec533aff7c192ee1
Matthew Smart <matthewsmart13@gmail.com>
2023-03-30 17:27:37 -0500

    Converted class comp to functional comp

--- src/App.js
+++ src/App.js
@@ -4,31 +4,28 @@ import EducationForm from "./components/EducationForm";
 import EmploymentForm from "./components/EmploymentForm";
 import GeneralForm from "./components/GeneralForm";
 
-class App extends Component {
-	constructor() {
-		super();
-	}
-
-	render() {
-		return (
-			<div className="App">
-				<header>
-					<h1>CV-App</h1>
-				</header>
-				<GeneralForm />
-				<EducationForm />
-				<EmploymentForm />
-				<footer>
-					<p>
-						Copyright © 2023{" "}
-						<a href="https://github.com/itsMe-ThatOneGuy" target="_blank">
-							itsMe-ThatOneGuy
-						</a>
-					</p>
-				</footer>
-			</div>
-		);
-	}
-}
+const App = () => {
+	return (
+		<div className="App">
+			<header>
+				<h1>CV-App</h1>
+			</header>
+			<GeneralForm />
+			<EducationForm />
+			<EmploymentForm />
+			<footer>
+				<p>
+					Copyright © 2023{" "}
+					<a
+						href="https://github.com/itsMe-ThatOneGuy"
+						target="_blank"
+						rel="noreferrer">
+						itsMe-ThatOneGuy
+					</a>
+				</p>
+			</footer>
+		</div>
+	);
+};
 
 export default App;
