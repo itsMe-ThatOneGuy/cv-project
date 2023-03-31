c7d7abb27e287b3f9f1f5529d1b91afa43953a33
Matthew Smart <matthewsmart13@gmail.com>
2023-03-30 22:14:34 -0500

    Converted class comps to functional comps

--- src/components/Employment.js
+++ src/components/Employment.js
@@ -1,32 +1,26 @@
-import { Component } from "react";
-
-class Employment extends Component {
-	render() {
-		return (
-			<div>
-				<h2>Employment Info</h2>
-				{this.props.info.jobs.map((job) => {
-					return (
-						<div key={job.id}>
-							<p>{job.employer}</p>
-							<p>Title: {job.title}</p>
-							<p>Responsibilities: {job.responsibilities}</p>
-							<p>Start Date: {job.startDate}</p>
-							<p>End Date: {job.endDate}</p>
-							<button onClick={this.props.editOnClick} id={job.id}>
-								Edit
-							</button>
-							<button
-								onClick={this.props.deleteOnclick}
-								id={`delete-${job.id}`}>
-								Delete
-							</button>
-						</div>
-					);
-				})}
-			</div>
-		);
-	}
-}
+const Employment = (props) => {
+	return (
+		<div>
+			<h2>Employment Info</h2>
+			{props.info.jobs.map((job) => {
+				return (
+					<div key={job.id}>
+						<p>{job.employer}</p>
+						<p>Title: {job.title}</p>
+						<p>Responsibilities: {job.responsibilities}</p>
+						<p>Start Date: {job.startDate}</p>
+						<p>End Date: {job.endDate}</p>
+						<button onClick={props.editOnClick} id={job.id}>
+							Edit
+						</button>
+						<button onClick={props.deleteOnclick} id={`delete-${job.id}`}>
+							Delete
+						</button>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
 
 export default Employment;
--- src/components/EmploymentForm.js
+++ src/components/EmploymentForm.js
@@ -1,66 +1,62 @@
-import { Component } from "react";
+import { useState } from "react";
 import Employment from "./Employment";
 import uniqid from "uniqid";
 
-class EmploymentForm extends Component {
-	constructor() {
-		super();
-
-		this.state = {
-			job: {
+const EmploymentForm = () => {
+	const [info, setInfo] = useState({
+		job: {
+			id: uniqid(),
+			employer: "",
+			title: "",
+			responsibilities: "",
+			startDate: "",
+			endDate: "",
+		},
+		jobs: [
+			{
 				id: uniqid(),
-				employer: "",
-				title: "",
-				responsibilities: "",
-				startDate: "",
-				endDate: "",
+				employer: "Test Employer",
+				title: "Full Stack Developer",
+				responsibilities: "Coding all the things",
+				startDate: "2020",
+				endDate: "2023",
 			},
-			jobs: [
-				{
-					id: uniqid(),
-					employer: "Test Employer",
-					title: "Full Stack Developer",
-					responsibilities: "Coding all the things",
-					startDate: "2020",
-					endDate: "2023",
-				},
-			],
-			displayForm: false,
-			edit: false,
-			edited: {
-				id: "",
-				employer: "",
-				title: "",
-				responsibilities: "",
-				startDate: "",
-				endDate: "",
-			},
-		};
+		],
+		displayForm: false,
+		edit: false,
+		edited: {
+			id: "",
+			employer: "",
+			title: "",
+			responsibilities: "",
+			startDate: "",
+			endDate: "",
+		},
+	});
 
-		this.editOnClick = this.editOnClick.bind(this);
-		this.deleteOnclick = this.deleteOnclick.bind(this);
-	}
-
-	openForm = () => {
-		this.setState({
+	const openForm = () => {
+		setInfo({
+			...info,
 			displayForm: true,
 		});
 	};
 
-	deleteOnclick = (e) => {
+	const deleteOnclick = (e) => {
 		const jobId = e.target.id.replace("delete-", "");
-		const newJobs = this.state.jobs.filter((job) => job.id != jobId);
-		this.setState({
+		const newJobs = info.jobs.filter((job) => job.id !== jobId);
+		setInfo({
+			...info,
 			jobs: newJobs,
 		});
 	};
 
-	editOnClick = (e) => {
+	const editOnClick = (e) => {
 		const jobId = e.target.id;
-		if (this.state.edit === true) return;
-		this.state.jobs.forEach((job) => {
+		if (info.edit === true) return;
+		info.jobs.forEach((job) => {
 			if (job.id === jobId) {
-				this.setState({
+				setInfo({
+					...info,
 					edit: true,
 					edited: {
 						id: jobId,
@@ -75,31 +71,33 @@ class EmploymentForm extends Component {
 		});
 	};
 
-	handelChange = (e) => {
-		if (this.state.edit === true) {
-			for (const [key] of Object.entries(this.state.edited)) {
+	const handelChange = (e) => {
+		if (info.edit === true) {
+			for (const [key] of Object.entries(info.edited)) {
 				if (e.target.id === `${key}Input`) {
-					this.setState((prevState) => ({
-						edited: { ...prevState.edited, [key]: e.target.value },
-					}));
+					setInfo({
+						...info,
+						edited: { ...info.edited, [key]: e.target.value },
+					});
 				}
 			}
 		} else {
-			for (const [key] of Object.entries(this.state.job)) {
+			for (const [key] of Object.entries(info.job)) {
 				if (e.target.id === `${key}Input`) {
-					this.setState((prevState) => ({
-						job: { ...prevState.job, [key]: e.target.value },
-					}));
+					setInfo({
+						...info,
+						job: { ...info.job, [key]: e.target.value },
+					});
 				}
 			}
 		}
 	};
 
-	onSubmitForm = (e) => {
+	const onSubmitForm = (e) => {
 		e.preventDefault();
-		if (this.state.edit === false) {
-			const jobs = this.state.jobs.concat(this.state.job);
-			this.setState({
+		if (info.edit === false) {
+			const jobs = info.jobs.concat(info.job);
+			setInfo({
 				job: {
 					id: uniqid(),
 					employer: "",
@@ -121,13 +119,11 @@ class EmploymentForm extends Component {
 				},
 			});
 		} else {
-			const old = this.state.jobs.find(
-				(item) => item.id === this.state.edited.id
-			);
-			const newJobs = this.state.jobs
-				.filter((item) => item.id != old.id)
-				.concat(this.state.edited);
-			this.setState({
+			const old = info.jobs.find((item) => item.id === info.edited.id);
+			const newJobs = info.jobs
+				.filter((item) => item.id !== old.id)
+				.concat(info.edited);
+			setInfo({
 				jobs: newJobs,
 				job: {
 					id: uniqid(),
@@ -148,104 +144,93 @@ class EmploymentForm extends Component {
 				},
 			});
 		}
-		console.log(this.state);
 	};
 
-	render() {
-		if (this.state.displayForm === true || this.state.jobs.length === 0) {
-			return (
-				<div>
-					<h3>Add New Employment Info</h3>
-					<form onSubmit={this.onSubmitForm}>
-						<label htmlFor="employerInput">Employer:</label>
-						<input
-							onChange={this.handelChange}
-							type="text"
-							id="employerInput"
-						/>
-						<label htmlFor="titleInput">Title:</label>
-						<input onChange={this.handelChange} type="text" id="titleInput" />
-						<label htmlFor="responsibilitiesInput">Responsibilities:</label>
-						<input
-							onChange={this.handelChange}
-							type="text"
-							id="responsibilitiesInput"
-						/>
-						<label htmlFor="startDateInput">Start Date:</label>
-						<input
-							onChange={this.handelChange}
-							type="text"
-							id="startDateInput"
-						/>
-						<label htmlFor="endDateInput">End Date:</label>
-						<input onChange={this.handelChange} type="text" id="endDateInput" />
-						<button type="submit">Save</button>
-					</form>
-					<Employment
-						info={this.state}
-						editOnClick={this.editOnClick}
-						deleteOnclick={this.deleteOnclick}
+	if (info.displayForm === true || info.jobs.length === 0) {
+		return (
+			<div>
+				<h3>Add New Employment Info</h3>
+				<form onSubmit={onSubmitForm}>
+					<label htmlFor="employerInput">Employer:</label>
+					<input onChange={handelChange} type="text" id="employerInput" />
+					<label htmlFor="titleInput">Title:</label>
+					<input onChange={handelChange} type="text" id="titleInput" />
+					<label htmlFor="responsibilitiesInput">Responsibilities:</label>
+					<input
+						onChange={handelChange}
+						type="text"
+						id="responsibilitiesInput"
 					/>
-					<button onClick={this.openForm}>Add More</button>
-				</div>
-			);
-		} else if (this.state.edit === true) {
-			return (
-				<div>
-					<h3>Edit Selected Employment Info</h3>
-					<form onSubmit={this.onSubmitForm}>
-						<label htmlFor="employerInput">Employer:</label>
-						<input
-							onChange={this.handelChange}
-							value={this.state.edited.employer}
-							type="text"
-							id="employerInput"
-						/>
-						<label htmlFor="titleInput">Title:</label>
-						<input
-							onChange={this.handelChange}
-							value={this.state.edited.title}
-							type="text"
-							id="titleInput"
-						/>
-						<label htmlFor="responsibilitiesInput">Responsibilities:</label>
-						<input
-							onChange={this.handelChange}
-							value={this.state.edited.responsibilities}
-							type="text"
-							id="responsibilitiesInput"
-						/>
-						<label htmlFor="startDateInput">Start Date:</label>
-						<input
-							onChange={this.handelChange}
-							value={this.state.edited.startDate}
-							type="text"
-							id="startDateInput"
-						/>
-						<label htmlFor="endDateInput">End Date:</label>
-						<input
-							onChange={this.handelChange}
-							value={this.state.edited.endDate}
-							type="text"
-							id="endDateInput"
-						/>
-						<button type="submit">Save</button>
-					</form>
-				</div>
-			);
-		} else {
-			return (
-				<div>
-					<Employment
-						info={this.state}
-						editOnClick={this.editOnClick}
-						deleteOnclick={this.deleteOnclick}
+					<label htmlFor="startDateInput">Start Date:</label>
+					<input onChange={handelChange} type="text" id="startDateInput" />
+					<label htmlFor="endDateInput">End Date:</label>
+					<input onChange={handelChange} type="text" id="endDateInput" />
+					<button type="submit">Save</button>
+				</form>
+				<Employment
+					info={info}
+					editOnClick={editOnClick}
+					deleteOnclick={deleteOnclick}
+				/>
+				<button onClick={openForm}>Add More</button>
+			</div>
+		);
+	} else if (info.edit === true) {
+		return (
+			<div>
+				<h3>Edit Selected Employment Info</h3>
+				<form onSubmit={onSubmitForm}>
+					<label htmlFor="employerInput">Employer:</label>
+					<input
+						onChange={handelChange}
+						value={info.edited.employer}
+						type="text"
+						id="employerInput"
 					/>
-					<button onClick={this.openForm}>Add More</button>
-				</div>
-			);
-		}
+					<label htmlFor="titleInput">Title:</label>
+					<input
+						onChange={handelChange}
+						value={info.edited.title}
+						type="text"
+						id="titleInput"
+					/>
+					<label htmlFor="responsibilitiesInput">Responsibilities:</label>
+					<input
+						onChange={handelChange}
+						value={info.edited.responsibilities}
+						type="text"
+						id="responsibilitiesInput"
+					/>
+					<label htmlFor="startDateInput">Start Date:</label>
+					<input
+						onChange={handelChange}
+						value={info.edited.startDate}
+						type="text"
+						id="startDateInput"
+					/>
+					<label htmlFor="endDateInput">End Date:</label>
+					<input
+						onChange={handelChange}
+						value={info.edited.endDate}
+						type="text"
+						id="endDateInput"
+					/>
+					<button type="submit">Save</button>
+				</form>
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<Employment
+					info={info}
+					editOnClick={editOnClick}
+					deleteOnclick={deleteOnclick}
+				/>
+				<button onClick={openForm}>Add More</button>
+			</div>
+		);
 	}
-}
+};
 
 export default EmploymentForm;
