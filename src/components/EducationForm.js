15267d9a26ae51e74a8bb8bea51c7b05e1588644
Matthew Smart <matthewsmart13@gmail.com>
2023-04-06 00:36:16 -0500

    Commented out functions to be refactored

--- src/components/EducationForm.js
+++ src/components/EducationForm.js
@@ -22,7 +22,10 @@ const EducationForm = () => {
 		});
 	};
 
+	//Need to change
+
 	const deleteOnclick = (e) => {
+		/*	
 		const schoolId = e.target.id.replace("delete-", "");
 		const newSchools = info.schools.filter(
 			(school) => school.id !== schoolId
@@ -31,9 +34,13 @@ const EducationForm = () => {
 			...info,
 			schools: newSchools,
 		});
+	*/
 	};
 
+	//Need to change
+
 	const editOnClick = (e) => {
+		/*	
 		const schoolId = e.target.id;
 		if (info.edit === true) return;
 		info.schools.forEach((school) => {
@@ -51,13 +58,17 @@ const EducationForm = () => {
 				});
 			}
 		});
+	*/
 	};
 
+	//Need to test
 	const handleChange = (key, value) => {
 		setBuffer({ ...buffer, [key]: value });
 	};
 
+	//need to change
 	const onSubmitForm = (e) => {
+		/*	
 		e.preventDefault();
 		if (info.edit === false) {
 			const schools = info.schools.concat(info.school);
@@ -104,6 +115,7 @@ const EducationForm = () => {
 				},
 			});
 		}
+	*/
 	};
 
 	if (displayForm === true || schools.length === 0) {
@@ -113,28 +125,28 @@ const EducationForm = () => {
 				<form onSubmit={onSubmitForm}>
 					<label htmlFor="schoolNameInput">School:</label>
 					<input
-						onChange={handelChange}
+						onChange={handleChange}
 						name="schoolName"
 						type="text"
 						id="schoolNameInput"
 					/>
 					<label htmlFor="degreeInput">Degree:</label>
 					<input
-						onChange={handelChange}
+						onChange={handleChange}
 						name="degree"
 						type="text"
 						id="degreeInput"
 					/>
 					<label htmlFor="startDateInput">Start Date:</label>
 					<input
-						onChange={handelChange}
+						onChange={handleChange}
 						name="startDate"
 						type="text"
 						id="startDateInput"
 					/>
 					<label htmlFor="endDateInput">End Date:</label>
 					<input
-						onChange={handelChange}
+						onChange={handleChange}
 						name="endDate"
 						type="text"
 						id="endDateInput"
@@ -147,47 +159,6 @@ const EducationForm = () => {
 					deleteOnclick={deleteOnclick}
 				/>
 				<button onClick={openForm}>Add More</button>
-			</div>
-		);
-	} else if (info.edit === true) {
-		return (
-			<div>
-				<h3>Edit Selected Education Info</h3>
-				<form onSubmit={onSubmitForm}>
-					<label htmlFor="schoolNameInput">School:</label>
-					<input
-						onChange={handleChange}
-						name="schoolName"
-						value={info.edited.schoolName}
-						type="text"
-						id="schoolNameInput"
-					/>
-					<label htmlFor="degreeInput">Degree:</label>
-					<input
-						onChange={handleChange}
-						value={info.edited.degree}
-						name="degree"
-						type="text"
-						id="degreeInput"
-					/>
-					<label htmlFor="startDateInput">Start Date:</label>
-					<input
-						onChange={handleChange}
-						name="startDate"
-						value={info.edited.startDate}
-						type="text"
-						id="startDateInput"
-					/>
-					<label htmlFor="endDateInput">End Date:</label>
-					<input
-						onChange={handleChange}
-						value={info.edited.endDate}
-						name="endDate"
-						type="text"
-						id="endDateInput"
-					/>
-					<button type="submit">Save</button>
-				</form>
 			</div>
 		);
 	} else {
