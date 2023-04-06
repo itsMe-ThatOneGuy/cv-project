689cf09ca9c438b4c03a80130df74b6e91d5ccc1
Matthew Smart <matthewsmart13@gmail.com>
2023-04-06 16:25:46 -0500

    Fixed: how the Data is structured in the modules

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,28 +1,20 @@
 import uniqid from "uniqid";
 
-const Data = () => {
-	const defaultSchoolData = () => {
-		return {
-			id: uniqid(),
-			schoolName: "Test School",
-			degree: "CIS",
-			startDate: "2013",
-			endDate: "2015",
-		};
-	};
-
-	const defaultEmploymentData = () => {
-		return {
-			id: uniqid(),
-			employer: "Test Employer",
-			title: "Full Stack Developer",
-			responsibilities: "Coding all the things",
-			startDate: "2020",
-			endDate: "2023",
-		};
-	};
-
-	return { defaultSchoolData, defaultEmploymentData };
+const defaultSchoolData = {
+	id: uniqid(),
+	schoolName: "Test School",
+	degree: "CIS",
+	startDate: "2013",
+	endDate: "2015",
 };
 
-export default Data;
+const defaultEmploymentData = {
+	id: uniqid(),
+	employer: "Test Employer",
+	title: "Full Stack Developer",
+	responsibilities: "Coding all the things",
+	startDate: "2020",
+	endDate: "2023",
+};
+
+export { defaultSchoolData, defaultEmploymentData };
